ab6034991a28c7e7c504a17aeb60fdcfee965830
coras-company-code <cora-jane.meade@cognizant.com>
2022-03-28 18:16:46 +0100

    Display articles

--- GoodNews/Views/Controllers/NewsListTableViewController.swift
+++ GoodNews/Views/Controllers/NewsListTableViewController.swift
@@ -10,15 +10,48 @@ import UIKit
 
 class NewsListTableViewController: UITableViewController {
     
+    private var articleListVM: ArticleListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
     }
     
     private func setup() { //private so that it can only be called in this class
-//        self.navigationController?.navigationBar.prefersLargeTitles = true //bar tint doesnt work with large titles - xcode bug
+        //  self.navigationController?.navigationBar.prefersLargeTitles = true
+        //  bar tint doesnt work with large titles - xcode bug
 
-        Webservice().fetchArticles(completion: { _ in})
+        Webservice().fetchArticles() { articles in
+            if let articles = articles {
+                
+                self.articleListVM = ArticleListViewModel(articles: articles)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+            
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM.numberOfRowsInSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell not found")
+        }
+        
+       let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
+        
+        cell.titleLabel.text = articleVM.title
+        cell.descriptionLabel.text = articleVM.description
+        return cell
     }
     
 }

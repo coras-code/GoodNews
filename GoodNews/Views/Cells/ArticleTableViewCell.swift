08395762accb6e207213f4fc55e21655671f304e
coras-company-code <cora-jane.meade@cognizant.com>
2022-03-28 15:32:47 +0100

    Add tableviewcell

---
+++ GoodNews/Views/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by M_931521 on 28/03/2022.
+//
+
+import Foundation
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
